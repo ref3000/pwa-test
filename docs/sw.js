3358f6f61a5330710e3ee86636597272d7566d66
yamamoto-kiyohiro <yamamoto_kiyohiro@cyberagent.co.jp>
2019-08-29 12:56:41 +0900

    wip

--- docs/sw.js
+++ docs/sw.js
@@ -1,32 +1,55 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.routing.registerRoute(
+  /\.js$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new workbox.strategies.NetworkFirst()
+);
+
 
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
 // キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-	'/test/',
-	'/test/main.css',
-	'/test/main.js'
-];
+// var CACHE_NAME = 'pwa-sample-caches';
+// var urlsToCache = [
+// 	'/test/',
+// 	'/test/main.css',
+// 	'/test/main.js'
+// ];
 
-// インストール処理
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then(function(cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
-});
+// // インストール処理
+// self.addEventListener('install', function(event) {
+// 	event.waitUntil(
+// 		caches
+// 			.open(CACHE_NAME)
+// 			.then(function(cache) {
+// 				return cache.addAll(urlsToCache);
+// 			})
+// 	);
+// });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches
-			.match(event.request)
-			.then(function(response) {
-				return response ? response : fetch(event.request);
-			})
-	);
-});
+// // リソースフェッチ時のキャッシュロード処理
+// self.addEventListener('fetch', function(event) {
+// 	event.respondWith(
+// 		caches
+// 			.match(event.request)
+// 			.then(function(response) {
+// 				return response ? response : fetch(event.request);
+// 			})
+// 	);
+// });
